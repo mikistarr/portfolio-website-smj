58e0a315a381d3ba2b78c3f38b9f26907594bb6b
mikistarr <shawnmichelle524@gmail.com>
2023-10-09 22:17:23 -0500

    Add script

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,45 @@
 console. log(
     "Welcome to My World")
+
+    function menuToggle() {
+        var x = document.getElementById('myNavtoggle');
+        if (x.className === 'navtoggle') {
+          x.className += ' responsive';
+        } else {
+          x.className = 'navtoggle';
+        }
+      }
+
+
+      // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+var modal = document.getElementById("myModal2");
+
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img02");
+var captionText = document.getElementById("caption2");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
